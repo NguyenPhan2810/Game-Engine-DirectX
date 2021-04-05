63a8fcc86bfa2c259ac56a0dbced0e1c16016870
Phan Quý Nguyên <pqn2810@gmail.com>
2021-04-05 13:47:22 +0700

    [Add]
    Describe swap chain

--- Engine/d3dApp.cpp
+++ Engine/d3dApp.cpp
@@ -1,6 +1,14 @@
 #include "d3dApp.h"
 
 D3DApp::D3DApp()
+: mDevice(nullptr)
+, mImmediateContext(nullptr)
+, mFullscreen(false)
+, m4xMsaaQuality(0)
+, mEnable4xMsaa(true)
+, mClientHeight(600)
+, mClientWidth(800)
+, mClientRefreshRate(144)
 {
 	InitDirect3D();
 }
@@ -39,11 +47,39 @@ bool D3DApp::InitDirect3D()
 #pragma endregion
 
 #pragma region Check 4X MSAA quality support
-
 	HR(mDevice->CheckMultisampleQualityLevels(
 		DXGI_FORMAT_R8G8B8A8_UNORM, 4, &m4xMsaaQuality));
 
 	assert(m4xMsaaQuality > 0);
+#pragma endregion
+
+#pragma region Create Swap chain
+
+	// Describe the swap chain
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = mClientWidth;
+	sd.BufferDesc.Height = mClientHeight;
+	sd.BufferDesc.RefreshRate = DXGI_RATIONAL{ mClientRefreshRate, 1 };
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	// 4X MSAA
+	if (!mEnable4xMsaa)
+		sd.SampleDesc = DXGI_SAMPLE_DESC{ 1, 0 };
+	else
+	{
+		sd.SampleDesc.Count = 4;
+		sd.SampleDesc.Quality = m4xMsaaQuality - 1; // Use maximum quality
+	}
+
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 1; // Double buffer
+	sd.OutputWindow = mhMainWnd;
+	sd.Windowed = !mFullscreen;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	sd.Flags = 0; // Default
+
 #pragma endregion
 
 	return true;
--- Engine/d3dApp.h
+++ Engine/d3dApp.h
@@ -11,8 +11,15 @@ protected:
 	bool InitDirect3D();
 
 protected:
+	HWND mhMainWnd;
 	UINT m4xMsaaQuality;
 
 	ID3D11Device* mDevice;
 	ID3D11DeviceContext* mImmediateContext;
+
+	bool mEnable4xMsaa;
+	int mClientWidth;
+	int mClientHeight;
+	bool mFullscreen;
+	UINT mClientRefreshRate;
 };
