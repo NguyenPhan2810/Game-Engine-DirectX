5efe8b1b2d3ac08c1d0d2ad8646890444965f96b
Phan Quý Nguyên <pqn2810@gmail.com>
2021-07-21 14:47:25 +0700

    Update main.cpp

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -5,15 +5,17 @@
 
 
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-	_In_ PSTR szCmdLine, _In_ int iCmdShow) 
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ PSTR szCmdLine, 
+	_In_ int iCmdShow) 
 {
 	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	DemoShowRoom theApp(hInstance);
+	DemoWave theApp(hInstance);
 
 	if (!theApp.Init())
 		return -1;
